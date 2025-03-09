d83a628b50aef47e2c485a524608e6b51a420860
charliestalberg667 <charlie.stalberg@icloud.com>
2025-03-09 17:22:28 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,69 +1,63 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Inter, Montserrat_Alternates } from "next/font/google";
 import Navbar from "@/components/navbar";
 import { LanguageProvider } from "@/components/language-provider";
 import { Toaster } from "@/components/ui/toaster";
 import Footer from "@/components/footer";
-import React, { StrictMode } from "react";
+import Script from "next/script";
+import {StrictMode} from "react";
 
+// Load fonts
 const inter = Inter({ subsets: ["latin"] });
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates",
+});
 
 export const metadata = {
   title: "SolarInstall",
   description:
-    "Installation de systèmes solaires | Installatie van zonne-energiesystemen",
+      "Installation de systèmes solaires | Installatie van zonne-energiesystemen",
   keywords:
-    "solar install, solaire, installation solaire, énergie renouvelable, panneaux solaires, solutions énergétiques, énergie durable, énergie verte, zonne-energie, zonne-installatie, hernieuwbare energie, zonnepanelen, energieoplossingen, duurzame energie, groene energie",
+      "solar install, solaire, installation solaire, énergie renouvelable, panneaux solaires, solutions énergétiques, énergie durable, énergie verte, zonne-energie, zonne-installatie, hernieuwbare energie, zonnepanelen, energieoplossingen, duurzame energie, groene energie",
   robots: "index, follow",
 };
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode;
 }) {
   return (
-    <StrictMode>
+      <StrictMode>
       <html lang="fr" suppressHydrationWarning>
-        <head>
-          {/* Métadonnées */}
-          <title>{metadata.title}</title>
-          <meta name="description" content={metadata.description} />
-          <meta name="keywords" content={metadata.keywords} />
-          <meta name="robots" content={metadata.robots} />
-
-          {/* Polices Google Fonts */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-            rel="stylesheet"
-          />
-        </head>
-        <body className={inter.className}>
-          <LanguageProvider>
-            <div className="flex flex-col min-h-screen">
-              <Navbar />
-              <main className="flex-grow">{children}</main>
-              <Footer />
-            </div>
-            <Toaster />
-          </LanguageProvider>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-05CXENLVGM"></script>
-          <script>
+      <head>
+        {/* Google Analytics */}
+        <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-05CXENLVGM"
+        />
+        <Script id="google-analytics">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-05CXENLVGM');
-          </script>
-
-        </body>
+          `}
+        </Script>
+      </head>
+      <body className={`${inter.className} ${montserratAlternates.variable}`}>
+      <LanguageProvider>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
+        <Toaster />
+      </LanguageProvider>
+      </body>
       </html>
-    </StrictMode>
+        </StrictMode>
   );
 }
